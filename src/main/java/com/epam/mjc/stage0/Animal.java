bd5995050ed0d263e1126ce6e0994512ffa8a1f4
i.shirinov <shirinovilyos6965@gmail.com>
2022-12-02 14:05:51 +0500

    Task #1. Class creation

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,23 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    String color;
+    int numberOfPaws;
+    boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        return ("This animal is mostly " + (color) + ". It has " + (numberOfPaws) + ((numberOfPaws > 1) ? " paws and " : " paw and ") + ((hasFur) ? "a" : "no") + " fur.");
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Animal("Yellow", 2, true);
+        System.out.println(animal.getDescription());
+
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,14 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
+    public Bird(String color, int numberOfPaws, boolean hasFur) {
+        super("blue", 2, false);
+
+    }
+
+    public String getDescription() {
+        return ("This animal is mostly " + (color) + ".It has " + (numberOfPaws) + ((numberOfPaws > 1) ? " paws and " : " paw and ") + ((hasFur) ? "a" : "no") + " fur.") + " Moreover, it has " + numberOfPaws + " wings and can fly.";
+    }
+
 
 }
